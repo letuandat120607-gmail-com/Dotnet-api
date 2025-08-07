2a50bbcf07ae2576dc539ea72be781e2f96be431
datang26622 <tienmntv00165@gmail.com>
2025-08-07 20:01:55 +0700

    yinss
    comitne

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ccb2b3c809ff8fb0e77981ba97a51b448a0f109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca860328f64cc8e1fed3ecc7654c7bc8bce11b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
